4fc055317e8515721bf9762efc35ce3ed0395026
Mathieu Lalonde <mrlalonde@live.ca>
2019-10-02 10:22:08 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule, MatIconModule, MatMenuModule} from '@angular/material';
+import { IpComponent } from './ip/ip.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IpComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
